6575f20d89a07cd9ccc2c8b3d4f4e3b5a54d9129
Chamelio <bhull009@gmail.com>
2026-04-30 22:58:26 -0400

    first commit

---
+++ 0-authentication.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.30.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "seir-2"
+  region  = "us-central1"
+}
